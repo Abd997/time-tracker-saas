311f5775c3b87a1805a089c1d80df073509fc3de
abdullah <abdullah>
2024-03-08 18:46:36 +0500

    chore: cleaned out project

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,43 @@
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "../../../../../lib/prisma";
 
 const authOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt'
     },
     providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        })
+    ],
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (!profile?.email) return false
 
-    ]
+            const user = await prisma.user.upsert({
+                where: {
+                    email: profile.email
+                },
+                create: {
+                    email: profile.email,
+                    name: profile?.name,
+                    tenant: {
+                        create: {
+                            name: "google"
+                        }
+                    }
+                },
+                update: {
+                    name: profile?.name
+                }
+            })
+
+            return true
+        },
+    }
 }
 
 const handler = NextAuth(authOptions)
